ccd35efea20f46dd806884457630d4f1f4038907
kirill050 <kirilltikhonov050@gmail.com>
2020-10-19 23:56:30 +0300

    Next try

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 
 int main() {
     char symbols[2];
-    int i; // НЕБЕЗОПАСНО1 : ПО УМОЛЧАНИЮ НЕ РАВНО НУЛЮ
+    int i;
     std::string *result = new std::string("");
 
     std::cout << "Enter 2 symbols to build a tree by them" << std::endl;
-    scanf("%c%c", &symbols[0], &symbols[1]); //НЕБЕЗОПАСНО 2
+    scanf("%c%c", &symbols[0], &symbols[1]);
     std::cout << symbols[0] << " " << symbols[1] << std::endl;
 
     (*result).push_back(symbols[0]);
@@ -22,5 +22,5 @@ int main() {
     }
 
     std::cout << *result;
-    return 0; //НЕБЕЗОПАСНО3 : УТЕЧКА ПАМЯТИ(НЕТ FREE())
+    return 0;
 }
